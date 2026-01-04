096aa9f0044212c27acfa46bc117ce6d022360d2
Austine <austinelomocso@gmail.com>
2026-01-04 09:04:05 +0800

    first commit

---
+++ src/main/java/org/example/deveros/DeverosApplication.java
@@ -0,0 +1,13 @@
+package org.example.deveros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeverosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeverosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/deveros/DeverosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.deveros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeverosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
